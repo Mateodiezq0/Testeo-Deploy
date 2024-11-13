dd4eed4de938f67c3da287e681bd2a5055ebaa7b
Beneyto Mateo <mateobeneyto@gmail.com>
2024-11-13 01:15:09 -0300

    Vemos pasar al lado del sv

--- service/backend.js
+++ service/backend.js
@@ -15,6 +15,17 @@ let eventos = [];
 let artistas = [];
 let usuario = '';
 
+const http = require("http");
+
+const server = http.createServer((req, res) =>{
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Hello world!")
+});
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => console.log("Server is running on port 3001"))
+
 app.use(cors()); // Permitir CORS
 // Middleware para analizar el cuerpo de la solicitud (JSON)
 app.use(bodyParser.json());
